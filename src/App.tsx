63ab2600ba2ea2e26a97d62cd28617c65bf47233
swp91 <seongwoo4477@gmail.com>
2024-03-18 19:38:47 +0900

    feat:로그인페이지 UI 작업

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TestPage } from "./pages/test/TestPage";
 import FindPasswordPage from "./pages/login/find_password/FindPasswordPage";
 import FindEmailPage from "./pages/login/find_email/FindEmailPage";
 import JoinPage from "./pages/join/JoinPage";
+import LoginPage from "./pages/login/Loginpage";
 
 interface RouteProps {
   path: string; // 라우트 경로
@@ -12,22 +13,22 @@ interface RouteProps {
 }
 
 function App() {
-
-  // 운영 환경 로깅 
-  console.log('NODE_ENV', import.meta.env.NODE);
+  // 운영 환경 로깅
+  console.log("NODE_ENV", import.meta.env.NODE);
 
   // api 서버 주소 로깅
-  console.log('SERVER_HOST', import.meta.env.VITE_SERVER_HOST);
+  console.log("SERVER_HOST", import.meta.env.VITE_SERVER_HOST);
 
-  // 라우트 매핑 설정 
+  // 라우트 매핑 설정
   // @TODO: 라우트 권한 설정
   const routes: Array<RouteProps> = [
     // ex :: { path: '/', component: MainPage, role: 'ROLE_ADMIN'},
-    { path: '/', component: MainPage },
-    { path: '/test', component: TestPage },
-    { path: '/login/find_password', component: FindPasswordPage },
-    { path: '/login/find_email', component: FindEmailPage },
-    { path: '/join', component: JoinPage },
+    { path: "/", component: MainPage },
+    { path: "/test", component: TestPage },
+    { path: "/login/find_password", component: FindPasswordPage },
+    { path: "/login/find_email", component: FindEmailPage },
+    { path: "/join", component: JoinPage },
+    { path: "/login", component: LoginPage },
   ];
 
   return (
@@ -35,12 +36,16 @@ function App() {
       <BrowserRouter>
         <Routes>
           {routes.map((route, index) => (
-            <Route key={index} path={route.path} element={<route.component />} />
+            <Route
+              key={index}
+              path={route.path}
+              element={<route.component />}
+            />
           ))}
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
