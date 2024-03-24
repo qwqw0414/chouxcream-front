e45c719ab764dc7c5f451e8e6f490f5f63f146d1
정화윤 <jeonghwayun0119@naver.com>
2024-03-24 23:13:50 +0900

    마이페이지 네비게이션 바 추가

---
+++ src/pages/mypage/my/address/AddressPage.tsx
@@ -0,0 +1,15 @@
+import ContentsWrapper from '@/components/layout/ContentsWrapper'
+import MyPageNav from '@/components/layout/MyPageNav'
+import React from 'react'
+
+const AddressPage: React.FC = () => {
+  return (
+    <ContentsWrapper>
+      <MyPageNav>
+        AddressPage
+      </MyPageNav>
+    </ContentsWrapper>
+  )
+}
+
+export default AddressPage
---
+++ src/pages/mypage/my/profileEdit/ProfileEditPage.tsx
@@ -0,0 +1,15 @@
+import ContentsWrapper from '@/components/layout/ContentsWrapper'
+import MyPageNav from '@/components/layout/MyPageNav'
+import React from 'react'
+
+const ProfileEditPage: React.FC = () => {
+  return (
+    <ContentsWrapper>
+      <MyPageNav>
+        ProfileEditPage
+      </MyPageNav>
+    </ContentsWrapper>
+  )
+}
+
+export default ProfileEditPage
