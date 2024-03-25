22c2f22ef6366a6c3db9f6c626941e9a7e756d81
jangyc <xzdi0414@gmail.com>
2024-03-25 19:47:20 +0900

    bugfix: - test page route

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -96,7 +96,7 @@ const Header: React.FC<HeaderProps> = ({ title, tabType }) => {
                                 HOME
                             </NavLink>
                             <NavLink
-                                to="/test/none"
+                                to="/test"
                                 end
                                 className={({ isActive }) => { return isActive ? "text-black font-bold" : ""; }}>
                                 TEST
